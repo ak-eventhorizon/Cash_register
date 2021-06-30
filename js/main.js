3d8579819f0a2f78d622f69b56330a0fc0c21ca0
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2021-07-01 00:48:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -258,3 +258,8 @@ ui.mainButton.addEventListener('click', calculateChange);
 
 // TODO -- MVC structure
 // разнести функционал по модулям, переделать на парадигму @ts-check и @param
+
+// 0
+// 1
+// 2 
+// 3
