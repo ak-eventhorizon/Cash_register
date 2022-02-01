9896bca8b55866947c22b2c93bc764494ebb9af1
ak-eventhorizon <45125426+ak-eventhorizon@users.noreply.github.com>
2022-02-02 00:10:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -260,4 +260,3 @@ ui.mainButton.addEventListener('click', calculateChange);
 // разнести функционал по модулям, переделать на парадигму @ts-check и @param
 
 // now - leather notebook time
-// and real notebook -- done!
